c8a1a805ecf19b3468986755a8203cfadda3acc9
December81 <megastudio@outlook.fr>
2025-02-20 10:57:52 +0100

    Mise à jour du Front de Base

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function DashboardUser() {
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 xl:grid-cols-4 px-8 py-8">
+        {/* Découper en Header */}
+
+        <h1 className="text-slate-200 text-2xl md:text-4xl font-bold tracking-tight">
+          Dashboard
+        </h1>
+
+        {/* Section d'actions */}
+      </div>
+    </>
+  );
+}
